7baaba11e6752cfcaf6027bba2b39b63446d5c97
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-01-20 20:33:07 +0200

    Controllers add|update|post|put

--- src/main/java/ua/tasklist/backspring/controller/CategoryController.java
+++ src/main/java/ua/tasklist/backspring/controller/CategoryController.java
@@ -1,6 +1,11 @@
 package ua.tasklist.backspring.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.tasklist.backspring.entity.Category;
@@ -24,8 +29,30 @@ public class CategoryController {
         this.categoryRepo = categoryRepo;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/all")
     public List<Category> test() {
         return this.categoryRepo.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestBody Category category) {
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity<>("redundant parameter:ID must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity<>("missing parameter: title must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(this.categoryRepo.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Category category) {
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity<>("parameter:ID must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity<>("missing parameter: title must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(this.categoryRepo.save(category));
     }
 }
--- src/main/java/ua/tasklist/backspring/controller/PriorityController.java
+++ src/main/java/ua/tasklist/backspring/controller/PriorityController.java
@@ -1,6 +1,11 @@
 package ua.tasklist.backspring.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.tasklist.backspring.entity.Priority;
@@ -24,10 +29,35 @@ public class PriorityController {
         this.priorityRepo = priorityRepo;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/all")
     public List<Priority> test() {
         final var all = this.priorityRepo.findAll();
         System.out.println("list " + all);
         return all;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity<>("redundant parameter:ID must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity<>("missing parameter: title must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(this.priorityRepo.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Priority priority) {
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity<>("parameter:ID must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity<>("missing parameter: title must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity<>("missing parameter: color must not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(this.priorityRepo.save(priority));
     }
 }
