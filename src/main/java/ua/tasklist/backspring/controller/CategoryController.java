27be70d201f8b47c112b0a73dcc0fd5986758f83
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-01-22 13:14:52 +0200

    @CrossOrigin

--- src/main/java/ua/tasklist/backspring/controller/CategoryController.java
+++ src/main/java/ua/tasklist/backspring/controller/CategoryController.java
@@ -3,6 +3,7 @@ package ua.tasklist.backspring.controller;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import java.util.Optional;
  */
 @RestController
 @RequestMapping("/category")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CategoryController {
     private final CategoryService service;
 
--- src/main/java/ua/tasklist/backspring/controller/StatController.java
+++ src/main/java/ua/tasklist/backspring/controller/StatController.java
@@ -2,6 +2,7 @@ package ua.tasklist.backspring.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.tasklist.backspring.entity.Stat;
@@ -18,6 +19,7 @@ import java.util.Optional;
  * @since 20.01.2021
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class StatController {
     private final StatService service;
 
